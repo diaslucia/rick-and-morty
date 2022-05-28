0b6423bd67d123518cf387a064989606e4c044d8
Lucia <lucy_17_4@hotmail.com>
2022-05-28 00:59:26 -0300

    Fix styles header and itemList

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import logo from "../../assets/logo.png";
 const Header = () => {
     return(
         <header>
-            <h1 className="header-title">Rick & Morty Characters</h1>
+            <h1 className="header-title">Rick and Morty characters</h1>
             <img className="header-logo" src={logo} alt="Logo Sirius"/>
         </header>
     )
